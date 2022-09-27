fa342ceb6d11cb0394df80218ec49683224fd7b8
Robert Loo <rloolee89@gmail.com>
2022-09-27 03:02:57 -0400

    Style input calorie calculator

--- src/components/Inputform/Inputform.js
+++ src/components/Inputform/Inputform.js
@@ -19,8 +19,7 @@ const [activity, setActivity] = useState(0);
 
 
 const options = [
-  { label: "Basal Metabolic Rate (BMR)", value: 0 },
-  { label: "Sedentary - no exercise", value: 1.2 },
+  { label: "Sedentary", value: 1.2 },
   { label: "Lightly Active", value: 1.375 },
   { label: "Moderately Active", value: 1.55 },
   { label: "Very Active", value: 1.725 },
@@ -39,14 +38,63 @@ const handleSubmit = (event) => {
   return (
     <div>
       <div className="inputform-container">
-        <div>
-          <h1>Calories Calculator</h1>
-          <p>To lose weight, you need to eat fewer calories than you expend over time. Thus, 
-            learning how many calories you burn every day is one of the most important steps you can take to lose weight.
-            Use this calculator to estimate how many calories you burn and how many you should eat every day to lose weight.</p>
+        <div className="calculator__container"> 
+          <div className="calculator__container-inner">
+            <h3 className="calculator__title">Calories Calculator</h3>
+            <p className="calculator__description">Use this calculator to estimate how many calories you burn and how many you should eat every day to lose weight, maintain, and gain muscle.
+            </p>
+          </div>
         </div>
-
+    <div className="calculator__container-lowerouter"> 
+      <div className="calculator__container-lowerinner"> 
         <form onSubmit={handleSubmit}>
+          <div className="gender__container">
+            <h3 className="gender__title">Gender </h3>
+          <div className="gender__container-right"> 
+              <input
+                type="radio"
+                name="gender"
+                id="male"
+                value={isMale}
+                onClick={() => setIsMale(true)}
+                checked
+              />
+              <label for="male">Male</label>
+              <input
+                type="radio"
+                name="gender"
+                id="female"
+                value={!isMale}
+                onClick={() => setIsMale(false)}
+              />
+              <label for="female">Female</label>
+            </div>
+          </div>
+
+        <div className="height__container-outer">
+          <div className="height__container">
+            <h3 className="height__title">Height</h3>
+            <input className="height__input"
+              value={height}
+              onChange={(event) => setHeight(event.target.value)}
+              type="number"
+              name="height"
+              placeholder="centimetres(cm)"
+            />
+          </div>
+          <div className="weight__container">
+            {/* <div> */}
+              <h3 className="weight__title">Weight </h3>
+              <input className="weight__input"
+                value={weight}
+                onChange={(event) => setWeight(event.target.value)}
+                type="number"
+                name="weight"
+                placeholder="kilograms(kg)"
+              />
+             {/* </div> */}
+          </div>
+        </div>
           <div>
             <h3>Age</h3>
             <input
@@ -57,58 +105,20 @@ const handleSubmit = (event) => {
             />
           </div>
           <div>
-            <h3>Gender </h3>
-            <input
-              type="radio"
-              name="gender"
-              id="male"
-              value={isMale}
-              onClick={() => setIsMale(true)}
-              checked
-            />
-            <label for="male">Male</label>
-            <input
-              type="radio"
-              name="gender"
-              id="female"
-              value={!isMale}
-              onClick={() => setIsMale(false)}
-            />
-            <label for="female">Female</label>
-          </div>
-          <div>
-            <h3>Height </h3>
-            <input
-              value={height}
-              onChange={(event) => setHeight(event.target.value)}
-              type="number"
-              name="height"
-              placeholder="cm"
-            />
-          </div>
-          <div>
-            <h3>Weight </h3>
-            <input
-              value={weight}
-              onChange={(event) => setWeight(event.target.value)}
-              type="number"
-              name="weight"
-              placeholder="kg"
-            />
-          </div>
-          <div>
           <Select options={options} onChange={(values) => setActivity(values[0].value)}/>
           <label for="activity">Activity Level</label>
           </div>
           <div>
            <button type="submit"
            onClick={(e) => handleSubmit(e)}
-           >Calculate</button>
+           >GET RESULTS!</button>
             <button type="submit">Clear</button>
           </div>
         </form>
       </div>
     </div>
+  </div>
+</div>
   );
 }
 
